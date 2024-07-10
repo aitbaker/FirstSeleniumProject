1a8b2fbd9d221cfe79c8513ad79acd356617028c
aitbaker <157494843+aitbaker@users.noreply.github.com>
2024-07-10 14:40:45 +0200

    Update FirstSeleniumTest.java

--- src/test/java/com/ait/tests/FirstSeleniumTest.java
+++ src/test/java/com/ait/tests/FirstSeleniumTest.java
@@ -8,25 +8,32 @@ import org.testng.annotations.Test;
 
 public class FirstSeleniumTest {
 
-    WebDriver driver;
+     WebDriver driver;
 
     //before - setUp
     @BeforeMethod
     public void setUp() {
-       driver = new ChromeDriver();
-       driver.get("https://www.google.com");
+        driver = new ChromeDriver();
+        //driver.get("https://www.google.com");
+
+        driver.navigate().to("https://www.google.com");//with history
+
+        driver.navigate().back();
+        driver.navigate().forward();
+        driver.navigate().refresh();
     }
 
     //tests
     @Test
-    public void findElementByTagName() {
-        System.out.println("Site opened!!!");
+    public void openGoogle() {
+        System.out.println("Google opened!");
     }
 
     //after - tearDown
-    @AfterMethod
+    @AfterMethod(enabled = false)
     public void tearDown() {
-        driver.quit();// all tabs & close browser
-        // driver.close(); only one tab(if tab only one -> close browser)
+        //interview question
+        driver.quit();//all tabs && close browser
+    //    driver.close();//only one tab(if only one tab -> close browser)
     }
 }
