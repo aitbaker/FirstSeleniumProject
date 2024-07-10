a3d3b0f7f67487e16bea9c3f00d968d74dcdded3
aitbaker <157494843+aitbaker@users.noreply.github.com>
2024-07-10 14:42:06 +0200

    Update FindElementsByLocatorsTests.java

--- src/test/java/com/ait/tests/FindElementsByLocatorsTests.java
+++ src/test/java/com/ait/tests/FindElementsByLocatorsTests.java
@@ -19,71 +19,136 @@ public class FindElementsByLocatorsTests {
         driver = new ChromeDriver();
 
         driver.get("https://ilcarro.web.app");
+        //maximize browser to window size
         driver.manage().window().maximize();
+        //wait for all elements on the site to load before start testing
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
-    @Test
-    public void startFindLocatorsTest() {
-        //id
-        driver.findElement(By.id("city"));
-        //className
-        driver.findElement(By.className("input-label"));
-        //name
-        driver.findElement(By.name("viewport"));
-        //linkText
-        WebElement element = driver.findElement(By.linkText("Let the car work"));
-        System.out.println(element.getText());
-        //partialLinkText
-        WebElement element1 = driver.findElement(By.partialLinkText("car"));
-        System.out.println(element1.getText());
-    }
-
-    @Test
-    public void startFindCssTest() {
-        //tagName = css
-        //driver.findElement(By.tagName("div"));
-        driver.findElement(By.cssSelector("div"));
-        //id -> css(#)
-        //driver.findElement(By.id("city"));
-        driver.findElement(By.cssSelector("#city"));
-        //className -> css(.)
-        // driver.findElement(By.className("input-label"));
-        driver.findElement(By.cssSelector(".input-label"));
-        //attribute -> css([])
-        driver.findElement(By.cssSelector("[href='/search']"));
-        driver.findElement(By.cssSelector("[for='dates']"));
-        //contains -> *
-        driver.findElement(By.cssSelector("[for*='ate']"));
-        //start -> ^
-        driver.findElement(By.cssSelector("[for^='dat']"));
-        //end on -> $
-        driver.findElement(By.cssSelector("[for$='tes']"));
-    }
-
-    @Test
-    public void startFindXpath() {
-        // -> //*[@attr='value']
-        //tagName = xpath(//)
-        //driver.findElement(By.tagName("div"));
-        driver.findElement(By.xpath("//div"));
-        //id -> xpath(//*[@id='value'])
-        //driver.findElement(By.id("city"));
-        driver.findElement(By.xpath("//*[@id='city']"));
-        //className -> xpath(//*[@class='value'])
-        // driver.findElement(By.className("input-label"));
-        driver.findElement(By.xpath("//*[@class='input-label']"));
-        //attribute -> xpath(//*[@attr='value'])
-        driver.findElement(By.xpath("//*[@href='/search']"));
-        driver.findElement(By.xpath("//*[@for='dates']"));
-        //start -> //*[starts-with(@attr,'...')]
-        driver.findElement(By.xpath("//*[starts-with(@for,'dat')]"));
-        //contains, end on -> //*[contains(.,'...')]
-        driver.findElement(By.xpath("//*[contains(.,'ate')]"));
-    }
-
-    @AfterMethod
+    @AfterMethod(enabled = true)
     public void tearDown() {
         driver.quit();
     }
+
+    @Test
+    public void findElementsByTagName() {
+
+        //find element by tag
+        WebElement element = driver.findElement(By.tagName("h1"));
+        System.out.println(element.getText());
+
+        WebElement element1 = driver.findElement(By.tagName("a"));
+        System.out.println(element1.getText());
+
+        //find elements by tag
+        List<WebElement> elements = driver.findElements(By.tagName("a"));
+        System.out.println(elements.size());
+    }
+
+    @Test
+    public void findElementByLocator() {
+        //by id
+        driver.findElement(By.id("city"));
+
+        //by class name
+        WebElement phone = driver.findElement(By.className("telephone"));
+        System.out.println(phone.getText());
+
+        //link text
+        WebElement linkText = driver.findElement(By.linkText("Let the car work"));
+        System.out.println(linkText.getText());
+
+        //partial link text
+        WebElement partialLinkText = driver.findElement(By.partialLinkText("work"));
+        System.out.println(partialLinkText.getText());
+    }
+
+    @Test
+    public void findElementByCssSelector() {
+        //tag name -> tag
+        //driver.findElement(By.tagName("h1"));
+        driver.findElement(By.cssSelector("h1"));
+
+        //id -> #id
+        // driver.findElement(By.id("city"));
+        driver.findElement(By.cssSelector("#city"));
+
+        //class -> .class
+        //driver.findElement(By.className("telephone"));
+        driver.findElement(By.cssSelector(".telephone"));
+
+        // [attr=''value]
+        WebElement cssSelector = driver.findElement(By.cssSelector("[ng-reflect-name='city']"));
+        System.out.println(cssSelector.getAttribute("type"));
+
+        //contains -> *
+        WebElement elementContains = driver.findElement(By.cssSelector("[ng-reflect-router-link*='car']"));
+        System.out.println(elementContains.getText());
+
+        //start -> ^
+        driver.findElement(By.cssSelector("[ng-reflect-router-link^='let']"));
+
+        //ent on -> $
+        driver.findElement(By.cssSelector("[ng-reflect-router-link$='work']"));
+    }
+
+    @Test
+    public void findElementByXpath() {
+        //   //*[@attr='value']
+
+        //tag name -> //tag
+        //driver.findElement(By.cssSelector("h1"));
+        driver.findElement(By.xpath("//h1"));
+
+        //id -> //*[@id='value']
+        //driver.findElement(By.cssSelector("#city"));
+        driver.findElement(By.xpath("//input[@id='city']"));
+
+        //class -> //*[@class='value']
+        //driver.findElement(By.cssSelector(".telephone"));
+        driver.findElement(By.xpath("//*[@class='telephone']"));
+
+        //text = //tag[text()='Text']
+        driver.findElement(By.xpath("//h2[text()='Type your data and hit Yalla!']"));
+        driver.findElement(By.xpath("//h2[.='Type your data and hit Yalla!']"));
+
+        //contains text -> //tag[contains(.,'Text')]
+        driver.findElement(By.xpath("//h2[contains(.,'Yalla!')]"));
+
+        //contains value -> //tag[contains(@attr,'partialValue')]
+        driver.findElement(By.xpath("//input[contains(@class,'target')]"));
+       // driver.findElement(By.xpath("//div/input[@id=\"UserName\\\"]"));
+
+        //start -> //tag[starts-with(@attr,'startOfValue')]
+        driver.findElement(By.xpath("//input[starts-with(@class,'ng-untouched')]"));
+    }
+
+    @Test
+    public void findElementByXpath2() {
+        //parent
+        driver.findElement(By.xpath("//h1/parent::*"));// move up
+        driver.findElement(By.xpath("//h1/.."));
+        driver.findElement(By.xpath("//h1/parent::div"));
+
+        //ancestor
+        driver.findElement(By.xpath("//h1/ancestor::*"));// all
+        driver.findElement(By.xpath("//h1/ancestor::div"));// two options
+        driver.findElement(By.xpath("//h1/ancestor::div[2]"));// one option
+
+        //following-sibling
+        driver.findElement(By.xpath("//h1/following-sibling::*"));// all
+        driver.findElement(By.xpath("//h1/following-sibling::form"));// one option
+
+        //preceding-sibling
+        driver.findElement(By.xpath("//h2/preceding-sibling::*"));
+    }
 }
+
+// css=div>span --> xpath=//div/span
+
+//css=div span --> xpath=//div//span
+
+//parent
+//child
+//ancestor
+//descendant
