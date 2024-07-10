4a39ac48679404a296f477d8a9c1320fff7065c1
aitbaker <157494843+aitbaker@users.noreply.github.com>
2024-07-10 15:34:50 +0200

    Create BaseHelper.java

---
+++ src/main/java/com/demoshop/fw/BaseHelper.java
@@ -0,0 +1,41 @@
+package com.demoshop.fw;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class BaseHelper {
+
+    WebDriver driver;
+
+    public BaseHelper(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public boolean isElementPresent(By locator) {
+        return driver.findElements(locator).size() > 0;
+    }
+
+    public void type(By locator, String text) {
+        if (text != null) {
+            click(locator);
+            driver.findElement(locator).clear();
+            driver.findElement(locator).sendKeys(text);
+        }
+    }
+
+    public void click(By locator) {
+        driver.findElement(locator).click();
+    }
+
+    public boolean isTextPresent(By locator, String text) {
+        List<WebElement> contacts = driver.findElements(locator);
+
+        for (WebElement element: contacts) {
+            if (element.getText().contains(text)) return true;
+        }
+        return false;
+    }
+}
